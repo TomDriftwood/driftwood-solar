c996403e59bc21d399cb399391c33a577a74a17d
valentinfrlch <valentin.froehlich@gmail.com>
2023-08-20 16:18:20 +0200

    merge

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// onready
+$(document).ready(function () {
+    // when clicking button 'hero-start-button'
+    $('#main-button').click(function () {
+        // open website (same tab)
+        window.open('https://www.notion.so/valentinfrlch/Selbstbau-Photovoltaikanlage-1ea59febff274d369490fd2e2843ac23', '_self');
+        console.log('button clicked');
+    });
+});
